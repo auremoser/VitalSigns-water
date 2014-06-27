b5bba646adf08ecfe4c3e2b8a51e462978616237
Aurelia Moser <auremoser@gmail.com>
2014-06-27 02:32:00 -0400

    spurf data removed, gauging stations match data, dates a bit weird

---
+++ js/water_data.js
@@ -0,0 +1,45 @@
+'use strict';
+var chart = new Highcharts.Chart({
+    chart: {
+        renderTo: 'container'
+    },
+    colors: [
+    // colors taken from the palette on this site: http://vitalsigns.org/
+    // '#FFCC33', '#4E733D', '#C04420', '#6E9962', '#9DCBDA', '#FBD8DB', '#fc8d59'
+    '#051036', '#6E9962', '#9DCBDA', '#FBD8DB', '#fc8d59'
+    ],
+    title: {
+        text: 'Environmental Reading at Gauging Station - TZA'
+    },
+    subtitle: {
+        text: 'rainfall, water-level, dmf, temperature'
+    },
+    credits: {
+    enabled: false
+    },
+    xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: { // don't display the dummy year
+            month: '%e. %b',
+            year: '%b'
+        },
+        title: {
+            text: 'Date'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Values'
+        },
+        min: 0
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br>',
+        pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+    },
+
+    series: [{
+        // Define the data points.
+        name: ''
+    }]
+});
